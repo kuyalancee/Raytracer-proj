3b6797f8d2bd8a3d6dcb428f0bb64a2b0d142531
kuyalancee <lancetrasporto@gmail.com>
2025-12-03 15:09:37 -0600

    Edit files to add diffuse sphere render, gamma-correction, etc. (Section 9)

--- camera.h
+++ camera.h
@@ -6,6 +6,8 @@ public:
 
     double aspect_ratio = 1.0;
     int image_width = 100;
+    int samples_per_pixel = 10;
+    int max_depth = 10;
 
     void render(const hittable &world) {
         initialize();
@@ -18,12 +20,12 @@ public:
             std::clog << "\rScanlines remaining: " << (image_height - j) <<
             " " << std::flush;
             for(int i = 0; i < image_width; i++){
-                auto pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
-                auto ray_direction = pixel_center - center;
-                ray r(center, ray_direction);
-
-                color pixel_color = ray_color(r, world);
-                write_color(std::cout, pixel_color);
+                color pixel_color(0, 0, 0);
+                for(int sample = 0; sample < samples_per_pixel; sample++) {
+                    ray r = get_ray(i, j);
+                    pixel_color += ray_color(r, max_depth, world);
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
             }
         }
 
@@ -32,6 +34,7 @@ public:
 
 private:
     int image_height;
+    double pixel_samples_scale;
     point3 center;
     point3 pixel00_loc;
     vec3 pixel_delta_u;
@@ -40,6 +43,8 @@ private:
     void initialize() {
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
+
+        pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = point3(0, 0, 0);
 
@@ -62,11 +67,32 @@ private:
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    color ray_color(const ray& r, const hittable& world) const {
+    ray get_ray(int i, int j) {
+        auto offset = sample_square();
+        auto pixel_sample = pixel00_loc
+                            + ((i + offset.x()) * pixel_delta_u)
+                            + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = center;
+        auto ray_direction = pixel_sample - ray_origin;
+
+        return ray(ray_origin, ray_direction);
+    }
+
+    vec3 sample_square() {
+        return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+    }
+
+    color ray_color(const ray& r, int depth, const hittable& world) const {
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
         hit_record rec;
 
-        if (world.hit(r, interval(0, infinity), rec)) {
-            return 0.5 * (rec.normal + color(1,1,1));
+        if (world.hit(r, interval(0.001, infinity), rec)) {
+            vec3 direction = rec.normal + random_unit_vector();
+            return 0.1 * ray_color(ray(rec.p, direction), depth - 1, world);
         }
 
         vec3 unit_direction = unit_vector(r.direction());
